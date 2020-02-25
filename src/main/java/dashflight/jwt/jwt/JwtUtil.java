ddc3d05b0072c085ddee0c6d097be38ddc5c5664
jbeck18 <j.beck18@me.com>
2020-02-25 17:18:54 -0500

    Added entrypoint

---
+++ src/main/java/dashflight/jwt/jwt/JwtUtil.java
@@ -0,0 +1,20 @@
+package dashflight.jwt.jwt;
+
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+
+public class JwtUtil {
+
+    private JwtVerifier jwtVerifier = new JwtVerifier();
+
+    public DecodedJWT verifySession(String token, String tokenFgp) {
+        DecodedJWT result = null;
+        try {
+            result = jwtVerifier.decodeJwtToken(token, tokenFgp);
+        } catch (JWTVerificationException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+}
